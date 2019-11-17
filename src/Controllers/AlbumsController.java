c7a3e6bfa28a08142c8f6d966c9bc76619e8e1e9
Victor Silva <victor.silva452@outlook.com>
2019-11-17 20:48:25 -0300

    Albums recived the base implementation

--- src/Controllers/AlbumsController.java
+++ src/Controllers/AlbumsController.java
@@ -6,6 +6,8 @@
 package Controllers;
 
 import Models.Albums;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +15,45 @@ import java.util.Map;
  *
  * @author victor
  */
-public class AlbumsController {
-    public Map<Integer, Albums> alumsRecorded = new HashMap<>();
+public class AlbumsController implements BaseController<Albums>{
+    public Map<Integer, Albums> albumsRecorded = new HashMap<>();
+    private Utils utils = new Utils();
+
+    @Override
+    public void insert(Albums element) {
+        albumsRecorded.put(utils.hashCode(), element);
+    }
+
+    @Override
+    public Object get() {
+        return albumsRecorded.values();
+    }
+
+    @Override
+    public Object getByKey(int key) {
+        return albumsRecorded.get(key);
+    }
+
+    @Override
+    public void update(int key, Albums element) {
+        albumsRecorded.replace(key, element);
+    }
+
+    @Override
+    public void remove(int key) {
+        albumsRecorded.remove(key);
+    }
+    
+    @Override
+    public Object[] getModelFields(){
+        ArrayList<String> payback = new ArrayList<>();
+        Field[] declaredFields = Albums.class.getDeclaredFields();
+        
+        for(Field field : declaredFields){
+            payback.add(field.getName());
+        }
+        
+        return payback.toArray();
+    }
 
 }
