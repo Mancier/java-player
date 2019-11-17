4d97e3f160e97128a2f192dba9b5c5891edc9310
Victor Silva <victor.silva452@outlook.com>
2019-11-17 20:54:18 -0300

    :rocket: Playlists has base implementation too

--- src/Controllers/PlaylistsController.java
+++ src/Controllers/PlaylistsController.java
@@ -6,6 +6,8 @@
 package Controllers;
 
 import Models.Playlists;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +15,44 @@ import java.util.Map;
  *
  * @author victor
  */
-public class PlaylistsController {
+public class PlaylistsController implements BaseController<Playlists>{
     public Map<Integer, Playlists> playlistsRecorded = new HashMap<>();
+    private Utils utils = new Utils();
 
+    @Override
+    public void insert(Playlists element) {
+        playlistsRecorded.put(utils.hashCode(), element);
+    }
+
+    @Override
+    public Object get() {
+        return playlistsRecorded.values();
+    }
+
+    @Override
+    public Object getByKey(int key) {
+        return playlistsRecorded.get(key);
+    }
+
+    @Override
+    public void update(int key, Playlists element) {
+        playlistsRecorded.replace(key, element);
+    }
+
+    @Override
+    public void remove(int key) {
+        playlistsRecorded.remove(key);
+    }
+    
+    @Override
+    public Object[] getModelFields(){
+        ArrayList<String> payback = new ArrayList<>();
+        Field[] declaredFields = Playlists.class.getDeclaredFields();
+        
+        for(Field field : declaredFields){
+            payback.add(field.getName());
+        }
+        
+        return payback.toArray();
+    }
 }
